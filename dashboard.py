34dfc80c1da036d0c1f1aab78d22c02a12c4094d
Administrator <administrator@localhost.localdomain>
2022-01-28 12:47:58 -0500

    added api root app route and configuration to listen to all address ports

--- dashboard.py
+++ dashboard.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
+def api_root():
+    return "Connection Successful"
+
+@app.route("/home")
 def home():
     return render_template("index.html")
     
 if __name__ == "__main__":
-    app.run() 
+    app.run(host="0.0.0.0") 
+    # accessible by any computer 
+    # on the network
